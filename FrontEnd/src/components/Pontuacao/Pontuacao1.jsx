ebce603c0b6c2369c6e9b0b0d2234f532c47cfbc
jnassula <jonatanassula@gmail.com>
2021-06-18 14:57:03 +0100

    Added components

---
+++ FrontEnd/src/components/Pontuacao/Pontuacao1.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Pontuacao.css';
+
+
+function Pontuacao1() {
+
+    return (
+        <div className="player1">
+            
+        </div>
+    )
+}
+
+
+export default Pontuacao1;
---
+++ FrontEnd/src/components/Pontuacao/Pontuacao2.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Pontuacao.css';
+
+
+function Pontuacao2() {
+
+    return (
+        <div className="player2">
+            
+        </div>
+    )
+}
+
+
+export default Pontuacao2;
