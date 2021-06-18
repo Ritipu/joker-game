59b4e074f8089c9beaae4fd96501479e70bc34b5
jnassula <jonatanassula@gmail.com>
2021-06-18 14:57:03 +0100

    Added components

---
+++ FrontEnd/src/components/Jokers/Jokers.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Jokers.css';
+
+
+function Jokers() {
+
+
+    return (
+        <div className="jokers">
+            
+        </div>
+    )
+
+}
+
+export default Jokers;
