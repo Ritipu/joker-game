c1b9bd3ae3c862fd31caccb8cb12ff004622ed45
Caweca105 <lemos.diogo105@gmail.com>
2021-07-01 14:16:41 +0100

    Update wss.js

--- BackEnd/wss.js
+++ BackEnd/wss.js
@@ -3,12 +3,24 @@ import * as fs from "fs";
 
 const wss = new WebSocket.Server({ port: 8080 });
 
+let clients = []
+const games = []
+
 wss.on('connection', function connection(ws) {
-    ws.on('message', function incoming(message) {
-      console.log('received: %s', message);
-    });
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+    if (message === 'startGame') {
+      clients.push(ws);
+      if (clients.length === 2) {
+        games.push
+        ({ 
+          jogadores: clients,
+          id: ""
+        })
+        clients = [];
+      }
+    } 
+    
+  });
   
-    fs.watch('contador.json', (data) => {
-        ws.send('update')
-    })
 });
